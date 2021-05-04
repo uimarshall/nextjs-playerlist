8c13a36c026ac5671d30478a0ad881c727f942e5
Marshall Akpan <uimarshall@gmail.com>
2021-05-04 13:41:38 +0100

    Initial file set up using next

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error,
+        praesentium?
+      </p>
+    </div>
+  );
+};
+
+export default About;
