3ef27d16f37fad3008606b2d79131ddcadf30ef0
Marshall Akpan <uimarshall@gmail.com>
2021-05-04 15:58:17 +0100

    Linking pages together using 'next/link'.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,19 @@
+import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Player List</h1>
       </div>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Player Listing</a>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/players">
+        <a>Player Listing</a>
+      </Link>
     </nav>
   );
 };
---
+++ pages/players/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Players = () => {
+  return (
+    <div>
+      <h1>All Players</h1>
+    </div>
+  );
+};
+
+export default Players;
