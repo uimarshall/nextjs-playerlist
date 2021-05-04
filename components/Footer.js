f4ce28e19f0867e1063f548fd5a4514a5b22f83a
Marshall Akpan <uimarshall@gmail.com>
2021-05-04 14:46:23 +0100

    Add resusable components, Navbar, Footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Copyright 2021 Player List</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Player List</h1>
+      </div>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Player Listing</a>
+    </nav>
+  );
+};
+
+export default Navbar;
